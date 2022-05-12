4deb71774c5b0bf20ffa29f8bff2a974492d6438
HenriqueGuima <lmarso3@gmail.com>
2022-05-12 10:36:17 +0100

    more changes

--- src/App.js
+++ src/App.js
@@ -10,23 +10,25 @@ function App() {
           <div className="col-md-12 coluna">
             {" "}
             <h1 className="text-center frase">
-              Uma nova cara, a qualidade de sempre!
+              Uma nova cara... a qualidade de sempre!
             </h1>
             <div className="sticker " />
-            <div className="container-fluid">
-              <div className="row contactos">
-                <div className="col-md-12 d-flex justify-content-between">
-                  <div className="col-md-3">
-                    <span>ARMAZÉM</span>{" "}
-                    <div>
-                      Rua da Agrela, Bouças
-                      <div> Apartado 278, 4824-909 Fafe</div>
+            <div className="container-fluid contact-width">
+              <div className="row contactos ">
+                <div className="container-fluid">
+                  <div className="d-flex justify-content-between">
+                    <div className="col-md-4">
+                      <span>ARMAZÉM</span>{" "}
+                      <div className="morada">
+                        Rua da Agrela, Bouças
+                        <div> Apartado 278, 4824-909 Fafe</div>
+                      </div>
                     </div>
-                  </div>
-                  <div className="col-md-3">
-                    <span>FÁBRICA</span>{" "}
-                    <div>
-                      Rua da Agrela<div>Apartado 128, 4820-403 Fafe</div>{" "}
+                    <div className="col-md-4 " style={{ textAlign: "right" }}>
+                      <span>FÁBRICA</span>{" "}
+                      <div className="morada">
+                        Rua da Agrela<div>Apartado 128, 4820-403 Fafe</div>{" "}
+                      </div>
                     </div>
                   </div>
                 </div>
